0c1a323b012afa6a43043fb9cfe66417ee7143d6
chandriga2011 <chandrigads@gmail.com>
2023-01-24 00:42:40 +0530

    new message

---
+++ src/org/test/Loop.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Loop {
+	
+	public static void main(String[] args) {
+		int a=10;
+		if (a==10) {
+			System.out.println("a is 10");
+					
+		}
+		int b=20;
+		if (b==10) {
+			System.out.println("b is 20");
+			
+		}
+	}
+
+}
