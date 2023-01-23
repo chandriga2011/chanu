9d0c79856e89451bfad37e85bfd0c2a1ebd03023
chandriga2011 <chandrigads@gmail.com>
2023-01-24 01:00:23 +0530

    New code

--- src/org/test/Loop.java
+++ src/org/test/Loop.java
@@ -6,7 +6,7 @@ public class Loop {
 		int a=10;
 		if (a==10) {
 			System.out.println("a is 10");
-			System.out.println("a  is equal to 10");
+			System.out.println("a  is  10");
 					
 		}
 		int b=20;
